871b578a525be269fb46b5cc6187d993b2e5d2c5
Elmehdi Salim <esalim@student.1337.ma>
2022-10-10 20:00:49 +0100

    solve MKO problem in ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 15:19:21 by esalim            #+#    #+#             */
-/*   Updated: 2022/10/10 19:27:53 by esalim           ###   ########.fr       */
+/*   Updated: 2022/10/10 19:54:12 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*d;
+	size_t	slen;
 
 	if (!s)
 		return (0);
+	slen = ft_strlen(s);
+	if (len > slen)
+		len = slen;
 	d = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!d)
 		return (0);
